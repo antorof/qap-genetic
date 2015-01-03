6c291d18878950e844b0b93378871dea2f4c6f3a
antorof <antorof.dev@gmail.com>
2015-01-03 02:15:02 +0100

    Clase Utils con función para generar permutaciones

---
+++ src/Utils.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 
+ * @author Antonio Toro
+ */
+public class Utils {
+	
+	/**
+	 * Genera una permutacion cuyos elementos estan dentro de un rango y de un
+	 * tamanio concreto.
+	 * @param min     Valor minimo de los elementos
+	 * @param max     Valor maximo de los elementos
+	 * @param tamanio Tamanio de la permutacion
+	 * @return La permutacion generada
+	 */
+	public static List<Integer> generarPermutacion(int min, int max, int tamanio) {
+		List<Integer> permutacion = new ArrayList<Integer>();
+		Random r = new Random();
+		int numero;
+		
+		while (permutacion.size() < tamanio) {
+			numero = r.nextInt(max - min) + min;
+			
+			if (!permutacion.contains(numero))
+				permutacion.add(numero);
+		}
+		
+		return permutacion;
+	}
+}
