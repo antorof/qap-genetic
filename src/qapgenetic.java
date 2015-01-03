54ab159a4eacf0e9f826d14a958db4ff9d7e5469
antorof <antorof.dev@gmail.com>
2015-01-03 14:38:10 +0100

    qapgenetic lanza la aplicación.

---
+++ src/qapgenetic.java
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+/**
+ * @author Antonio Toro
+ */
+public class qapgenetic {
+
+	public static void main(String[] args) {
+		if(args.length != 2) {
+			System.err.println("N\u00FAmero de argumentos incorrectos");
+			System.err.println("Uso: java " + qapgenetic.class.getName() + " <fichero> <tama\u00F1o poblaci\u00F3n>");
+			System.exit(1);
+		}
+		
+		String fichero = args[0];
+		int tamPoblacion = Integer.parseInt(args[1]);
+		ArrayList<Cromosoma> poblacion = new ArrayList<Cromosoma>();
+		
+		// Se crean los casos de prueba a partir del fichero
+		Casos casos = null;
+		try {
+			casos = new Casos(fichero);
+		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+			System.err.println("No se encuentra el archivo");
+			System.exit(2);
+		}
+	
+		// Se crea la poblacion aleatoriamente
+		for(int i = 0; i < tamPoblacion; i++)
+			poblacion.add(new Cromosoma(Utils.generarPermutacion(0, casos.getTamanio(), casos.getTamanio())));
+
+//		AlgoritmoGenetico ag = new AlgoritmoGenetico(casos, poblacion);
+//		ag.ejecutarAlgoritmo();
+
+	}
+
+}
